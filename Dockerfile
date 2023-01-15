6c50344cae04a8e1b3470bd18c0bbcdd52f6c3c0
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-01-14 22:21:35 -0600

    Add a Dockerfile and support for unix sockets.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM rust:1.66 AS builder
+WORKDIR /app
+COPY . .
+# COPY Cargo.toml Cargo.toml
+# COPY Cargo.lock Cargo.lock
+# COPY src/ src/
+
+RUN cargo install --path .
+
+FROM debian:buster-slim
+RUN apt-get update -y
+COPY --from=builder /usr/local/cargo/bin/ws-markdown-server /usr/local/bin/ws-markdown-server
+
+ENV RUST_LOG="ws_markdown_server=debug"
+ENV MD_SERVER_WS_PORT="9003"
+ENV MD_SERVER_TCP_PORT="9004"
+ENV MD_SERVER_HOST="0.0.0.0"
+
+ENTRYPOINT ["ws-markdown-server"]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use clap::Parser;
 use comrak::{markdown_to_html, ComrakOptions};
-
 use futures_util::{SinkExt, StreamExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::WebSocketStream;
+use tracing::metadata::LevelFilter;
+use tracing_subscriber::EnvFilter;
 use std::net::ToSocketAddrs;
 use std::{
     net::SocketAddr,
@@ -8,7 +12,24 @@ use std::{
 };
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::tungstenite::Result;
-use tracing::{error, info, trace};
+use tracing::{error, info, trace, debug};
+
+
+#[derive(Parser, Debug, Clone)]
+#[clap(
+    author="Aalekh Patel <aalekh.gwpeck.7998@icloud.com>", 
+    about="A simple markdown-to-html websocket server backed by comrak and tokio-tungstenite.", 
+    version="1.0.0",
+)]
+
+pub struct Opts {
+    #[clap(short, long, default_value = "0.0.0.0", env = "MD_SERVER_HOST")]
+    pub host: String,
+    #[clap(short, long, default_value_t = 9093, env = "MD_SERVER_WS_PORT")]
+    pub ws_port: u16,
+    #[clap(short, long, default_value = None, env = "MD_SERVER_TCP_PORT")]
+    pub port: Option<u16>,
+}
 
 #[derive(Debug, Clone, Default)]
 pub struct MarkdownEngine {
@@ -21,9 +42,9 @@ impl MarkdownEngine {
     }
 
     #[inline(always)]
-    #[tracing::instrument]
+    #[tracing::instrument(level = "trace", skip(self))]
     pub fn render(&self, md: &str) -> String {
-        trace!(text_len = md.len(), "Rendering markdown...");
+        debug!(text_len = md.len(), "Rendering markdown...");
         markdown_to_html(md, &self.options)
     }
 }
@@ -31,12 +52,15 @@ impl MarkdownEngine {
 pub type SharedState<T> = Arc<Mutex<T>>;
 pub type Engine = SharedState<MarkdownEngine>;
 
-pub async fn accept_connection(
+pub async fn accept_ws_connection(
     stream: TcpStream,
     engine: Engine,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = stream.peer_addr()?;
-    if let Err(e) = handle_connection(addr, stream, engine).await {
+    let ws_stream = tokio_tungstenite::accept_async(stream).await?;
+    debug!("Accepted WS connection from {}", addr);
+
+    if let Err(e) = handle_ws_connection(addr, ws_stream, engine).await {
         match e {
             tokio_tungstenite::tungstenite::Error::ConnectionClosed
             | tokio_tungstenite::tungstenite::Error::AlreadyClosed
@@ -51,12 +75,23 @@ pub async fn accept_connection(
     Ok(())
 }
 
-pub async fn handle_connection(peer: SocketAddr, stream: TcpStream, engine: Engine) -> Result<()> {
-    let mut ws_stream = tokio_tungstenite::accept_async(stream).await?;
-    info!("Accepted connection from {}", peer);
+pub async fn accept_socket_connection(
+    stream: TcpStream,
+    engine: Engine,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = stream.peer_addr()?;
+    debug!("Accepted WS connection from {}", addr);
+
+    if let Err(e) = handle_socket_connection(addr, stream, engine).await {
+        error!("Error handling connection from {}: {}", addr, e);
+    }
+    Ok(())
+}
+
+
+pub async fn handle_ws_connection(peer: SocketAddr, mut ws_stream: WebSocketStream<TcpStream>, engine: Engine) -> Result<()> {
     while let Some(msg) = ws_stream.next().await {
         let msg = msg?;
-        trace!("Received message from {}: {:?}", peer, msg);
         if msg.is_text() || msg.is_binary() {
             trace!("Received text/binary message from {}: {:?}", peer, msg);
             let msg = msg.to_text()?;
@@ -67,24 +102,69 @@ pub async fn handle_connection(peer: SocketAddr, stream: TcpStream, engine: Engi
     Ok(())
 }
 
-pub async fn create_server<A: ToSocketAddrs>(
+pub async fn handle_socket_connection(peer: SocketAddr, mut stream: TcpStream, engine: Engine) -> Result<()> {
+    debug!("Accepted TcpStream from {}", peer);
+    let (mut read_half, mut write_half) = stream.split();
+    loop {
+        let mut buf = String::new();
+        let n = read_half.read_to_string(&mut buf).await?;
+        if n == 0 {
+            return Ok(());
+        }
+        let outbound_msg = engine.lock().unwrap().render(&buf);
+        write_half.write_all(outbound_msg.as_bytes()).await?;
+    }
+}
+
+pub async fn create_ws_server<A: ToSocketAddrs>(
     addr: A,
     engine: Engine,
 ) -> Result<(), Box<dyn std::error::Error>> {
+
     let addr = addr.to_socket_addrs()?.next().unwrap();
     let listener = TcpListener::bind(&addr).await?;
+    info!("Listening for WebSocket connections on {}", addr);
 
-    info!("Listening on {}", addr);
     while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(stream, engine.clone()));
+        tokio::spawn(accept_ws_connection(stream, engine.clone()));
+    }
+    Ok(())
+}
+
+
+pub async fn create_tcp_server<A: ToSocketAddrs>(
+    addr: A,
+    engine: Engine,
+) -> Result<(), Box<dyn std::error::Error>> {
+
+    let addr = addr.to_socket_addrs()?.next().unwrap();
+    let listener = TcpListener::bind(&addr).await?;
+    info!("Listening for TCP (Unix) socket connections on {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_socket_connection(stream, engine.clone()));
     }
     Ok(())
 }
 
 #[tokio::main]
-pub async fn main() {
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opts = Opts::parse();
     tracing_subscriber::fmt::init();
-    let addr = "0.0.0.0:9003";
     let engine = Arc::new(Mutex::new(MarkdownEngine::new()));
-    create_server(addr, engine).await.unwrap()
+    let ws_addr = format!("{}:{}", opts.host, opts.ws_port);
+
+    let task1 = create_ws_server(ws_addr, engine.clone());
+
+    let task2 = opts.port.map(|port| {
+        let addr = format!("{}:{}", opts.host, port);
+        create_tcp_server(addr, engine.clone())
+    });
+
+    if let Some(task2) = task2 {
+        _ = tokio::join!(task1, task2);
+    } else {
+        task1.await?;
+    }
+    Ok(())
 }
